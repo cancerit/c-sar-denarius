9a6f871be6781c0153cc474b20bbb2ed1e44356d
Keiran Raine <kr2@sanger.ac.uk>
2021-08-10 15:20:32 +0100

    Add skywalking-eyes license checking

--- tests/02_serve_test.py
+++ tests/02_serve_test.py
@@ -1,3 +1,32 @@
+#
+# Copyright (C) 2021
+#
+# Author: CASM/Cancer IT <cgphelp@sanger.ac.uk>
+#
+# This file is part of c-sar-denarius.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# 1. The usage of a range of years within a copyright statement contained within
+# this distribution should be interpreted as being equivalent to a list of years
+# including the first and last year specified and all consecutive years between
+# them. For example, a copyright statement that reads ‘Copyright (c) 2005, 2007-
+# 2009, 2011-2012’ should be interpreted as being identical to a statement that
+# reads ‘Copyright (c) 2005, 2007, 2008, 2009, 2011, 2012’ and a copyright
+# statement that reads ‘Copyright (c) 2005-2012’ should be interpreted as being
+# identical to a statement that reads ‘Copyright (c) 2005, 2006, 2007, 2008,
+# 2009, 2010, 2011, 2012’.
 import os
 from stat import *
 
@@ -30,12 +59,15 @@ for k, v in YAML_PERMS.items():
     ],
 )
 def test_01_check_permissions(auth_file, exp_exit, exp_result):
+    auth_path = os.path.join(DATA_DIR, auth_file)
     if exp_exit:
         with pytest.raises(SystemExit) as e_info:
-            serve.check_permissions(os.path.join(DATA_DIR, auth_file))
+            serve.check_permissions(auth_path)
     else:
-        serve.check_permissions(os.path.join(DATA_DIR, auth_file))
+        serve.check_permissions(auth_path)
         assert True
+    # put permissions back for user
+    os.chmod(auth_path, S_IRUSR | S_IWUSR)
 
 
 @pytest.mark.parametrize(
