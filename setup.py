9b3224e2365b52aa176f07e74586df98ea06f005
Keiran Raine <kr2@sanger.ac.uk>
2021-09-14 13:26:02 +0100

    changes to support new c-sar version, update this version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ config = {
     "author": "Keiran M Raine",
     "url": "https://github.com/cancerit/c-sar-denarius",
     "author_email": "cgphelp@sanger.ac.uk",
-    "version": "0.1.0",
+    "version": "1.0.0",
     "license": "AGPL-3.0",
     "python_requires": ">= 3.9",
     "install_requires": [
